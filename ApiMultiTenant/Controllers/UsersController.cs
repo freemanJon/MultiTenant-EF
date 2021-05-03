acee9456d3553b825f0f18686c005fcf29cc1ee1
Jonatan Freidmon <46496338+freemanJon@users.noreply.github.com>
2021-05-03 12:32:31 -0300

    Update UsersController.cs

--- ApiMultiTenant/Controllers/UsersController.cs
+++ ApiMultiTenant/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using ApiMultiTenant.Data;
+using ApiMultiTenant.Models;
 using ApiMultiTenant.Service;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,5 +25,33 @@ namespace ApiMultiTenant.Controllers
             return Ok(users);
 
         }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Users user){
+            if(user is null)
+                return BadRequest();
+            _userService.Add(user);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id){
+            var user = _userService.GetById(id);
+            if(user is null)
+                return NotFound();
+            _userService.Delete(user);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Users user){
+            var userUpdate = _userService.GetById(id);
+            if(userUpdate is null)
+                return NotFound();
+            userUpdate.name = user.name;
+            _userService.Update(userUpdate);
+            return Ok();
+        }
+
     }
 }
